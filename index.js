d5d3e3bd4ab200e34ffa290b925281e191ab4d51
Juseong Kim <khackskjs@gmail.com>
2021-08-25 00:36:50 +0900

    Set express initial setting

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 require('./config/environment')
+
+const express = require('express')
+const app = express()
+const port = process.env.SERVER_PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})

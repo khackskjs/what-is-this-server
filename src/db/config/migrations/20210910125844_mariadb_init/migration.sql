d1b5345aa44857319f4f04f2f774b1736bff2d65
Juseong Kim <khackskjs@gmail.com>
2021-09-10 22:30:07 +0900

    Change card db scheme

--- src/db/config/migrations/20210910125844_mariadb_init/migration.sql
+++ src/db/config/migrations/20210910125844_mariadb_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE `User` (
-    `uid` INTEGER NOT NULL AUTO_INCREMENT,
+    `uuid` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191),
     `studyDateCount` INTEGER DEFAULT 1,
@@ -8,14 +8,14 @@ CREATE TABLE `User` (
     `updatedAt` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `User.email_unique`(`email`),
-    PRIMARY KEY (`uid`)
+    PRIMARY KEY (`uuid`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `CardGroup` (
     `guid` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
-    `uid` INTEGER NOT NULL,
+    `uuid` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -28,6 +28,10 @@ CREATE TABLE `Card` (
     `guid` INTEGER,
     `text1` VARCHAR(191) NOT NULL,
     `text2` VARCHAR(191) NOT NULL,
+    `reviewStage` INTEGER NOT NULL DEFAULT 0,
+    `lastReviewResult` INTEGER NOT NULL DEFAULT 0,
+    `dateOfReview` INTEGER,
+    `dateForNextReview` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -35,7 +39,7 @@ CREATE TABLE `Card` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `CardGroup` ADD FOREIGN KEY (`uid`) REFERENCES `User`(`uid`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `CardGroup` ADD FOREIGN KEY (`uuid`) REFERENCES `User`(`uuid`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Card` ADD FOREIGN KEY (`guid`) REFERENCES `CardGroup`(`guid`) ON DELETE SET NULL ON UPDATE CASCADE;
